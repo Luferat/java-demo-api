d2f0ed6393f0fba371d1f42893a1c4a221876e66
André Luferat <contato@luferat.net>
2023-06-12 21:43:46 -0300

    dfghjkl

---
+++ src/main/java/net/luferat/java/demoapi1/TrecoController.java
@@ -0,0 +1,22 @@
+package net.luferat.java.demoapi1;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trecos")
+public class TrecoController {
+	
+	@Autowired
+	private TrecoRepository trecoRepository;
+	
+	@GetMapping
+	public List<Treco> getAll(){
+		return trecoRepository.findAll();
+	}
+
+}
---
+++ src/main/java/net/luferat/java/demoapi1/TrecoRepository.java
@@ -0,0 +1,7 @@
+package net.luferat.java.demoapi1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrecoRepository extends JpaRepository<Treco, Long> {
+
+}
