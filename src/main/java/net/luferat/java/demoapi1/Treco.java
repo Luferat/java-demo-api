489d771e07b62551da073e21cb66c9cf3ea41777
Luferat <contato@luferat.net>
2023-06-13 11:35:20 -0300

    Ajustes e bug do Lombok

--- src/main/java/net/luferat/java/demoapi1/Treco.java
+++ src/main/java/net/luferat/java/demoapi1/Treco.java
@@ -5,14 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor
-@Getter
 @Setter
+@Getter
 public class Treco {
 
 	@Id
