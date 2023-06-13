919334033bd16303f33d57ed2343ebb3f2802960
Luferat <contato@luferat.net>
2023-06-13 10:54:28 -0300

    Update TrecoController.java

--- src/main/java/net/luferat/java/demoapi1/TrecoController.java
+++ src/main/java/net/luferat/java/demoapi1/TrecoController.java
@@ -3,13 +3,7 @@ package net.luferat.java.demoapi1;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,37 +17,6 @@ public class TrecoController {
 	@GetMapping
 	public List<Treco> getAll() {
 		return trecoRepository.findAll();
-	}
-
-	@PostMapping
-	public Treco post(@RequestBody Treco treco) {
-		return trecoRepository.save(treco);
-	}
-
-	@GetMapping("/{id}")
-	public Treco getOne(@PathVariable Long id) {
-		if (trecoRepository.existsById(id))
-			return trecoRepository.findById(id).get();
-		return null;
-	}
-
-	@DeleteMapping(path = "/{id}", produces = "application/json")
-	public String delete(@PathVariable Long id) {
-		if (trecoRepository.existsById(id)) {
-			trecoRepository.deleteById(id);
-			return "{\"status\":\"deleted\"}";
-		}
-		return "{\"status\":\"error\"}";
-	}
-
-	@PutMapping("/{id}")
-	public Treco put(@PathVariable Long id, @RequestBody Treco treco) {
-		return null;
-	}
-
-	@PatchMapping()
-	public Treco patch(@PathVariable Long id, @RequestBody Treco treco) {
-		return null;
 	}
 
 }
