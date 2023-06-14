95a21e9f0cd355184b694e3ec2367f7c3fa052a3
André Luferat <contato@luferat.net>
2023-06-13 21:42:19 -0300

    Update TrecoController.java

--- src/main/java/net/luferat/java/demoapi1/TrecoController.java
+++ src/main/java/net/luferat/java/demoapi1/TrecoController.java
@@ -3,9 +3,18 @@ package net.luferat.java.demoapi1;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/trecos")
@@ -17,6 +26,44 @@ public class TrecoController {
 	@GetMapping
 	public List<Treco> getAll() {
 		return trecoRepository.findAll();
+	}
+
+	@GetMapping(path = "/{id}", produces = "application/json")
+	public String getOne(@PathVariable Long id) {
+		if (trecoRepository.existsById(id)) {
+			ObjectMapper mapper = new ObjectMapper();
+			Treco treco = trecoRepository.findById(id).get();
+			try {
+				return mapper.writeValueAsString(treco);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
+		return "{ \"status\" : \"not found\" }";
+	}
+
+	@PostMapping
+	public Treco post(@RequestBody Treco treco) {
+		return trecoRepository.save(treco);
+	}
+
+	@DeleteMapping(path = "/{id}", produces = "application/json")
+	public String delete(@PathVariable Long id) {
+		if (trecoRepository.existsById(id)) {
+			trecoRepository.deleteById(id);
+			return "{ \"status\" : \"deleted\" }";
+		}
+		return "{ \"status\" : \"error\" }";
+	}
+
+	@PutMapping(path = "/{id}")
+	public Treco put(@PathVariable Long id, @RequestBody Treco treco) {
+		return null;
+	}
+
+	@PatchMapping(path = "/{id}")
+	public Treco patch(@PathVariable Long id, @RequestBody Treco treco) {
+		return null;
 	}
 
 }
