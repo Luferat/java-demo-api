8f87938b69d5d180ea668409d94c1c53d3ef15a0
André Luferat <contato@luferat.net>
2023-06-15 21:41:39 -0300

    Update Treco.java

--- src/main/java/net/luferat/java/demoapi1/treco/Treco.java
+++ src/main/java/net/luferat/java/demoapi1/treco/Treco.java
@@ -1,6 +1,6 @@
 package net.luferat.java.demoapi1.treco;
 
-import jakarta.persistence.Column;
+	import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
